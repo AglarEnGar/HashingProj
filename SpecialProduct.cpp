540f29fe3d2df9ef7c47560d02c9015bd2ea71fd
AglarEnGor <45472326+AglarEnGar@users.noreply.github.com>
2023-12-04 13:18:59 -0800

    deleting unecessarry complication
    e

--- SpecialProduct.cpp
+++
@@ -1,8 +0,0 @@
-#include "SpecialProduct.h"
-
-SpecialProduct::SpecialProduct(int id, const std::string &name, double price, const std::string &feature)
-    : Product(id, name, price), specialFeature(feature) {}
-
-std::string SpecialProduct::getSpecialFeature() const {
-  return specialFeature;
-}
