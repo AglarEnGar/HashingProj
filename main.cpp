c201e81f9c969409604ee6ef81ae007239e5d262
Luca <luca.j.morgan@gmail.com>
2023-12-05 01:15:08 -0800

    Functional Loading Function
    we can now load .cvs files into the inventory hashmap.
    the inventory hashmap is indexed with string keys of the product's name.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,54 @@
 #include <iostream>
 #include <hash_map>
 #include <string>
+#include <fstream>
 #include "main.h"
+#include <regex>
+
+#define DEFAULT_INVENTORY "inventory_list.csv"
+#define DEFAULT_OUTPUT_FILE "inventory_update.csv"
+
+int max_menu_option = 10;
+int max_inventory_file = 5;
 
 using namespace std;
 
-std::string Product::getDescription() const{ return (*this).description; }
-int Product::getProductId() const { return (*this).productId; }
-std::string Product::getProductName() const{  return (*this).productName; }
-double Product::getProductPrice() const { return (*this).productPrice; }
+const std::regex comma(",");
 
-void Product::setDescription(std::string str) { (*this).description = str; }
-void Product::setProductId(int id) { (*this).productId = id; }
-void Product::setProductName(std::string str) { (*this).productName = str; }
-void Product::setProductPrice(double price) { (*this).productPrice = price; }
-
-
+Inventory * store::loadFileintoInv(std::string file)
+{
+    auto * p = new Product;
+    auto * inv = new Inventory;
+    std::string line = "";
+    ifstream input_file;
+    input_file.open(file);
 
 
+    if(!input_file.is_open())
+    {
+        cout << "ERROR! Cannot read chosen file " << file << ". File \"" << 1 << "\" remains open." << endl;
+        return loadFileintoInv(mainInvFile);
+    }
 
-
-
+    while(input_file && getline(input_file, line))
+    {
+        std::vector<std::string> row { std::sregex_token_iterator(line.begin(),line.end(),comma,-1), std::sregex_token_iterator() };
+        p->setProductId(stoi(row.at(0)));
+        p->setProductName(row.at(1));
+        p->setProductPrice(std::stod(row.at(2)));
+        p->setDescription(row.at(3));
+        inv->insert(*p);
+    }
+    return inv;
+}
 
 int main() {
-    std::string word = "ooga";
-    std::size_t hash_booger = std::hash<std::string>{}(word);
 
-    std::cout << "Lol hi";
+    Inventory * inv = new Inventory;
+    store prompt;
+
+    inv = prompt.loadFileintoInv(DEFAULT_INVENTORY);
+    inv->printall();
 
     return 0;
 }
--- main.h
+++ main.h
@@ -12,7 +12,7 @@
 
 struct Product{
 private:
-    int productId = 0;
+    int productId;
     std::string productName;
     double productPrice;
     std::string description;
@@ -29,6 +29,7 @@ public:
     void setDescription(std::string str);
     void setProductId(int id);
     void setProductPrice(double price);
+    void printProduct();
 
     bool operator==(const Product& other) const
     {
@@ -38,34 +39,62 @@ public:
                 description == other.getDescription());
     }
 };
-
-template <>
-struct std::hash<Product>
+Product::Product()
 {
-    std::size_t operator()(const Product &p) const noexcept
-    {
-        std::size_t build = 17;
-        build = build * 31 + std::hash<std::string>()( p.getDescription() );
-        build = build * 31 + std::hash<std::string>()( p.getProductName() );
-        build = build * 31 + std::hash<int>()( p.getProductId() );
-        build = build * 31 + std::hash<double>()( p.getProductPrice() );
-        return build;
-    }
+    (*this).productId =0;
+    (*this).productName = "";
+    (*this).productPrice = 0.0;
+    (*this).description = "Lorem Ipsum";
 
-};
+}
+Product::~Product()
+= default;
+
+std::string Product::getDescription() const{ return (*this).description; }
+int Product::getProductId() const { return (*this).productId; }
+std::string Product::getProductName() const{  return (*this).productName; }
+double Product::getProductPrice() const { return (*this).productPrice; }
+
+void Product::setDescription(std::string str1) { (*this).description = str1; }
+void Product::setProductId(int id) { (*this).productId = id; }
+void Product::setProductName(std::string str2) { (*this).productName = str2; }
+void Product::setProductPrice(double price) { (*this).productPrice = price; }
+
+void Product::printProduct()
+{
+    std::cout   << "\"" << (*this).productName << "\"" << " | "
+                << "Price: $" << (*this).productPrice << " | "
+                << "Details: " << (*this).description << " | "
+                << "S/N: " << (*this).productId << std::endl;
+}
+
+//template <>
+//struct std::hash<Product>
+//{
+//    std::size_t operator()(const Product &p) const noexcept
+//    {
+//        std::size_t build = 17;
+//        build = build * 31 + std::hash<std::string>()( p.getDescription() );
+//        build = build * 31 + std::hash<std::string>()( p.getProductName() );
+//        build = build * 31 + std::hash<int>()( p.getProductId() );
+//        build = build * 31 + std::hash<double>()( p.getProductPrice() );
+//        return build;
+//    }
+//
+//};
 
 class Inventory{
 private:
-    std::unordered_map<int, Product> in1;
-    int size = 0;
+    std::unordered_map<std::string, Product> in1;
 
 public:
     Inventory();
     ~Inventory();
-    Product getFromId(int id);
-    void setFromId(int id);
-    void insert(Product p);
-
+    Product getFromName(std::string name) const;
+    void delFromName(std::string name);
+    void insert(const Product &p);
+    void printall();
+    unsigned int size() const;
 };
 
 Inventory::Inventory()
@@ -76,11 +105,35 @@ Inventory::~Inventory()
     std::destroy((*this).in1.begin(), (*this).in1.end());
 }
 
-void Inventory::insert(Product p)
+Product Inventory::getFromName(std::string name) const
 {
-    in1.insert({p.getProductId(), p});
+    auto search = in1.find(name);
+    return search->second;
 }
 
+void Inventory::delFromName(std::string name)
+{
+    in1.erase(in1.find(name));
+}
+
+void Inventory::insert(const Product &p)
+{
+    in1.insert({p.getProductName(), p});
+}
+
+void Inventory::printall()
+{
+    for (auto & it1 : in1)
+    {
+        it1.second.printProduct();
+
+    }
+}
+
+unsigned int Inventory::size() const
+{
+    return (*this).in1.size();
+}
 
 class CartItem {
 private:
@@ -108,12 +161,35 @@ private:
 public:
     CartItem* getHead();
     CartItem* getTail();
-    int size();
+    int length();
 
     void setHead(CartItem* newHead);
     void setTail(CartItem* newTail);
 
     void push_back(CartItem* newItem);
 };
+
+class store {
+private:
+    std::string mainInvFile = "inventory_list.csv";
+    std::string mainOutFile = "inventory_update.csv";
+public:
+    store();
+    ~store();
+    int menuOptions();
+
+    Inventory * loadFileintoInv(std::string file);
+    void loadItemsFromInv(Product c1);
+    void outputCartintoFile(std::string ofile);
+    void outputInvintoFile(std::string oofile);
+    void insertionSortCart();
+    void mergeSortCart();
+    void quicksortCart();
+};
+
+store::store() = default;
+store::~store() = default;
+
+
 
 #endif //HASHING_MYHASHING_H
